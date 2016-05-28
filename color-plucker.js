d9b487ddab8a2b3c9b2eb1bcd072b25b06139cd8
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-05-28 16:09:53 -0700

    implemented color picker logic

---
+++ color-plucker.js
@@ -0,0 +1,43 @@
+//get canvas
+var canvas =  document.getElementById('color-plucker').getContext('2d');
+
+//add image to page in canvas
+var img = new Image();
+img.src = 'image.jpg';
+$(img).load(function(){
+  canvas.drawImage(img, 0, 0);
+});
+
+// thanks javascripter http://www.javascripter.net/faq/rgbtohex.htm
+function rgbToHex(R,G,B) {
+  return toHex(R)+toHex(G)+toHex(B);
+};
+
+function toHex(n) {
+  n = parseInt(n,10);
+  if (isNaN(n)) return '00';
+  n = Math.max(0,Math.min(n,255));
+  return '0123456789ABCDEF'.charAt((n-n%16)/16) + '0123456789ABCDEF'.charAt(n%16);
+};
+
+//set up click event to get canvas form page location less canvas offsets
+$('#color-plucker').click(function(event){
+  var x = event.pageX - this.offsetLeft;
+  var y = event.pageY - this.offsetTop;
+
+// get the RGB value of location of click event from canvas object
+  var imgData = canvas.getImageData(x, y, 1, 1 ).data;
+
+  var R = imgData[0];
+  var G = imgData[1];
+  var B = imgData[2];
+
+  var rgb = R + ', ' + G + ', ' + B;
+  $('#rgb input').val(rgb);
+
+//convert RGB to HEX
+  var hex = rgbToHex(R,G,B);
+
+//add hex to page
+  $('#hex input').val('#' + hex);
+});
